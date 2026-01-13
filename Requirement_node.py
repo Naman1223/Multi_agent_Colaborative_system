85d670ed615a4d84f76ed130f69d5b4b000bf4dd
Namant1223 <namant1223@gmail.com>
2026-01-13 18:27:17 +0530

    origin

--- Requirement_node.py
+++ Requirement_node.py
@@ -1,23 +1,22 @@
 from typing import TypedDict , List 
 from langgraph.graph import StateGraph ,START , END
 from models import Reasoning_llm,Planning_llm , UI_llm
-
+from langgraph.checkpoint.memory import InMemorySaver
+import tools
 class AgentState(TypedDict):
     requirement: List[str]
 
-user_input = input("Enter your requirement: ")
-def reasoning_node(state: AgentState) -> AgentState:
-    # Extract the latest requirement or input
-    input_text = state['requirement'][-1]
-    reasoning = UI_llm(input_text)
-    state['requirement'].append(reasoning)
+requirement = tools.pdf_to_markdown("")
+
+def requirement_node(state: AgentState) -> AgentState:
+    state['requirement'].append(requirement)
     return state
 
+checkpointer= InMemorySaver()
 graph = StateGraph(AgentState)
-graph.add_node("reasoning_node", reasoning_node)
-graph.add_edge(START, "reasoning_node")
-graph.add_edge("reasoning_node", END)
-agent = graph.compile()
-result = agent.invoke({"requirement": [user_input]})
+graph.add_node("requirement_node", requirement_node)
+graph.add_edge(START, "requirement_node")
+graph.add_edge("requirement_node", END)
+agent = graph.compile(checkpointer=checkpointer)
+result = agent.invoke({"requirement": [requirement]}, config={"configurable": {"thread_id": "1"}})
 print(result)
-
